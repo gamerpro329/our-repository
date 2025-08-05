48704f7409d68493bc69acc9cc0da0dcca0d437f
GitHub Action <action@github.com>
2025-08-05 09:18:09 +0000

    Initialize template repository proper
    Set package name to com.github.gamerpro329.ourrepository
    Set project name to our-repository
    Set modid to ourrepository

--- src/main/java/com/github/gamerpro329/ourrepository/ExampleMod.java
+++ src/main/java/com/github/gamerpro329/ourrepository/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.gamerpro329.ourrepository;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "ourrepository", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/gamerpro329/ourrepository/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/gamerpro329/ourrepository/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.gamerpro329.ourrepository.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
